1b50d0296d625874e2ff3b46a89e7f411333ef2d
Raman Sehgal <sc.ramansehgal@gmail.com>
2021-11-06 20:48:59 +0530

    Displaying data in tabular format.

--- web/view.php
+++ web/view.php
@@ -9,27 +9,45 @@ $conn = new mysqli($servername, $username, $password,$dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo nl2br("Connected successfully\n");
+//echo nl2br("Connected successfully\n");
+
 return $conn;
 }
 
+echo "<center><h2>ISMRAN Data Status</h2></center>";
 $servername = "127.0.0.1";
-$username = "ismran";
-$password = "ismran123";
-$dbname="ismran_db";
+//$username = "ismran";
+$username = "barcq";
+//$password = "ismran123";
+$password = "barcq";
+//$dbname="ismran_db";
+$dbname="barcq";
 $conn=Connect($servername,$username,$password,$dbname);
 $sqlQuery="SELECT * from ismran_files";
 $results=$conn->query($sqlQuery);
 //echo $results."\n";
+echo "<center><table border='1'>";
+echo "<tr bgcolor='yellow'><th>Source Path </th><th>Destination Path</th> <th>File Name</th>";
+//echo "<th>File Integrity</th>;
+echo "</tr>";
 if($results->num_rows > 0){
 while($row=$results->fetch_assoc()){
+if($row['integrityCheck']==1)
+echo "<tr bgcolor='#56de91'>";
+else
+echo "<tr>";
+echo "<td>".$row["filePath"]."</td>";
+echo "<td>".$row['remoteFilePath']."</td>";
+echo "<td><a href='".$row['remoteFilePath']."/".$row['fileName']."'>".$row['fileName']."</a></td>";
+//echo "<td><center>".$row['integrityCheck']."</center></td>";
 //echo $row."\n";
-echo nl2br("FileName : ".$row["hashCode"]."\n");
+//echo nl2br("FileName : ".$row["hashCode"]."\n");
 //echo "raman <br/>";
+echo "</tr>";
 }
 }else{
 echo "0 results";
 }
-
+echo "</table></center>";
 $conn->close();
 ?> 
