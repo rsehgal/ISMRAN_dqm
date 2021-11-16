bf49e0a34291de564c20885fec3d5421f144c32f
Raman Sehgal <rsehgal@megh-os-102849.megh.barc.gov.in>
2021-11-16 15:54:04 +0530

    Added feature to do Integrity check on remote machine and NOT on DAQ machine.

---
+++ inc/Helpers.h
@@ -0,0 +1,16 @@
+/*
+**	Filename : Helpers.h
+**	2021-11-16
+**	username : rsehgal
+*/
+#ifndef Helpers_h
+#define Helpers_h
+
+#include <string>
+#include <vector>
+namespace ismran {
+extern std::vector<std::string> GetStringTokens(std::string pathString,char delim='/');
+extern std::string GetPath_StartingFromMountPoint(std::string pathString);
+extern std::string GetAmbarMountPoint_ParentDir();
+} // namespace ismran
+#endif
---
+++ test/core/Test_Tokenizing.cpp
@@ -0,0 +1,23 @@
+/*
+**	Filename : Test_Tokenizing.cpp
+**	2021-11-16
+**	username : rsehgal
+*/
+/*
+** 	Demo to tokenize a string (filePath)
+*/
+#include "Helpers.h"
+#include <iostream>
+#include <string>
+#include <vector>
+int main(int argc, char *argv[]) {
+
+  std::string filePath = "/home/rsehgal/test/abct.txt";
+  std::vector<std::string> tokensVector = ismran::GetStringTokens(filePath);
+
+  for (unsigned int i = 0; i < tokensVector.size(); i++) {
+    std::cout << tokensVector[i] << std::endl;
+  }
+
+  return 0;
+}
