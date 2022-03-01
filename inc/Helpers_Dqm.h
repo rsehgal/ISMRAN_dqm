5550bd169b0f8e3c46461e417b3e78008ab2eb94
rsehgal <sc.ramansehgal@gmail.com>
2022-03-01 14:20:57 +0530

    Modified code to copy the rate plots at correct location on AMBAR
    ------------------------------------------------------------------
    For eg.
    Source dir : /home/neutrino/IsmranDataA/Dhruva/March2022/images
    Target dir : /home/neutrino/AmbarData/Dhruva/March2022/images
    ------------------------------------------------------------------

--- inc/Helpers_Dqm.h
+++ inc/Helpers_Dqm.h
@@ -10,8 +10,12 @@
 #include <vector>
 namespace ismran {
 extern std::vector<std::string> GetStringTokens(std::string pathString,char delim='/');
+extern std::string GetStringToken_WithIndex(std::string pathString,char delim='/', unsigned short tokenIndex=0);
 extern std::string GetPath_StartingFromMountPoint(std::string pathString);
+extern std::string GetPath_StartingFrom_DelimiterWithIndex(std::string pathString,char delim='_',unsigned short delimIndex=1);
+extern std::string GetCopyPath_UptoMonth(std::string pathString);
 extern std::string GetAmbarMountPoint_ParentDir();
 extern std::string GetRemoteDataDirectory_OnDAQMachine();
+extern bool FileExists (const std::string& name);
 } // namespace ismran
 #endif
--- src/Helpers_Dqm.cpp
+++ src/Helpers_Dqm.cpp
@@ -22,10 +22,42 @@ std::vector<std::string> GetStringTokens(std::string pathString, char delim) {
   return tokensVector;
 }
 
+std::string GetStringToken_WithIndex(std::string pathString, char delim, unsigned short tokenIndex ) {
+  std::vector<std::string> tokensVector;
+  std::stringstream ss(pathString);
+  std::string token;
+  while (getline(ss, token, delim)) {
+    tokensVector.push_back(token);
+  }
+  return tokensVector[0];
+}
+
+std::string GetPath_StartingFrom_DelimiterWithIndex(std::string pathString, char delim, unsigned short delimIndex){
+  std::vector<std::string> tokensVector = GetStringTokens(pathString,delim);
+  std::string reqPath = "";
+  std::string s(1,delim);
+  std::cout << "Delimiter Converted to String : " << s << std::endl;
+  for (unsigned int i = delimIndex+1; i < tokensVector.size(); i++) {
+    reqPath += s+tokensVector[i];
+  }
+  std::cout << "returning string  : " << reqPath << std::endl;
+  return reqPath;
+}
+
+
 std::string GetPath_StartingFromMountPoint(std::string pathString) {
   std::vector<std::string> tokensVector = GetStringTokens(pathString);
   std::string reqPath = "";
   for (unsigned int i = 3; i < tokensVector.size(); i++) {
+    reqPath += "/"+tokensVector[i];
+  }
+  return reqPath;
+}
+
+std::string GetCopyPath_UptoMonth(std::string pathString) {
+  std::vector<std::string> tokensVector = GetStringTokens(pathString);
+  std::string reqPath = "";
+  for (unsigned int i = 0; i < tokensVector.size()-1; i++) {
     reqPath += "/"+tokensVector[i];
   }
   return reqPath;
@@ -41,4 +73,12 @@ std::string GetRemoteDataDirectory_OnDAQMachine() {
   return copyPath;
 }
 
+bool FileExists (const std::string& name) {
+    if (FILE *file = fopen(name.c_str(), "r")) {
+        fclose(file);
+        return true;
+    } else {
+        return false;
+    }
+}
 } // namespace ismran
